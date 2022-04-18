74053899aff2fb9f409bb9ed17abbece69545546
Aakash Kumar <64356169+Aakashk14@users.noreply.github.com>
2022-04-18 16:04:42 +0530

    Create export_worker.js
    worker to export reports

---
+++ main/export_worker.js
@@ -0,0 +1,52 @@
+// worker thread to export
+
+const { workerData,parentPort, isMainThread} = require('worker_threads')
+const mongoose = require('mongoose')
+console.log("exporting")
+mongoose.connect('mongodb://localhost:27017/interview',{autoIndex:true})
+
+const interview_db = require("../database/queries/interviews")
+const fs = require('fs')
+const org = workerData
+var empty=0;
+
+interview_db.interviews_all(org).then((result)=>{
+    console.log("got result",result)
+
+   
+ let temp="Department,Name,email,Status,Applied Position \n"
+
+if(result!=0){
+
+for(let i=0;i<result[0].departments.length;i++){
+
+    for(let j=0;j<result[0].departments[i].Interviews.length;j++){
+        empty=1;
+
+        temp=temp+result[0].departments[i].Name+",";
+        temp=temp+result[0].departments[i].Interviews[j].Name+",";
+        temp=temp+result[0].departments[i].Interviews[j].email+",";
+        temp=temp+result[0].departments[i].Interviews[j].Status+",";
+        temp=temp+result[0].departments[i].Interviews[j].Applied_for+",";
+        temp=temp+"\n"
+
+    }
+    
+}
+}
+
+if(empty!=0){
+fs.writeFile(`./Storage/Orgs/${org}/export.csv`,temp,(err)=>{
+    if(err){}
+})
+
+parentPort.postMessage({status:'done'})
+}else{
+    console.log("am i here")
+    parentPort.postMessage({status:'error'})
+}
+
+}).catch((error)=>{
+    console.log(error)
+})
+
