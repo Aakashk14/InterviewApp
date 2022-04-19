f988ce65993663e786c373f9bf87220f3881bb9e
Aakash Kumar <64356169+Aakashk14@users.noreply.github.com>
2022-04-19 14:26:33 +0530

    Update admins.js

--- database/modules/admins.js
+++ database/modules/admins.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const admin_schema = new mongoose.Schema({
     Name:String,
-    email:String,
+    email:{type:String,unique:true},
     password:String,
     userid:Number,
     level:Boolean,  //0- admin, 1- emp
